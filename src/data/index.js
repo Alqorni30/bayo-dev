edeb84006476347c46d56e66c03358fef0c207b6
Alqorni30 <vinnbayo@gmail.com>
2023-12-11 22:33:13 +0700

    push project

--- src/data/index.js
+++ src/data/index.js
@@ -6,249 +6,294 @@ import course from "../assets/projects/Course.png";
 import bimbel from "../assets/projects/Bimbel.png";
 import umkm from "../assets/projects/Umkm.png";
 import notes from "../assets/projects/notesapp.png";
-import manga from "../assets/projects/manga.png"
+import manga from "../assets/projects/manga.png";
 import react from "../assets/react.svg";
-import undangan from "../assets/projects/Undangan.png"
-import bayodev from "../assets/projects/bayodev.png"
-
+import undangan from "../assets/projects/Undangan.png";
+import bayodev from "../assets/projects/bayodev.png";
+import weblkg from "../assets/projects/web lkg.png";
 
 export const allProject = [
+  {
+    id: 1,
+    image: movie,
+    alt: "movie",
+    title: "Bayo film",
+    desc: "Slicing Web Movie with React js.",
+    tech1: react,
+    alt1: "react",
+    tech2:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+    alt2: "boostrap",
+    tech3:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    alt3: "javascript",
+    link: "https://bayocrisypy.netlify.app/",
+    preview: "Preview",
+  },
 
-    {
-        id: 1,
-        image: movie,
-        alt: "movie",
-        title: "Bayo film",
-        desc: "Slicing Web Movie with React js.",
-        tech1: react,
-        alt1: "react",
-        tech2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
-        alt2: "boostrap",
-        tech3: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-        alt3: "javascript",
-        link: "https://bayocrisypy.netlify.app/",
-        preview: "Preview"
-    },
-    
-    {
-        id: 2,
-        image: umkm,
-        alt: "umkm",
-        title: "Crispy.Id",
-        desc: "Pesan cemilan favoritmu diwebsite ini",
-        tech1: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
-        alt1: "html", 
-        tech2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
-        alt2: "css",
-        tech3: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-        alt3: "javascript",
-        link: "https://bayocrisypy.netlify.app/",
-        preview: "Preview"
-    },
-    {
-        id: 6,
-        image: nft,
-        alt: "nft",
-        title: "Tweak AI",
-        desc: "Company Profile memamerkan Bermacam-macam NFT",
-        tech1: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
-        alt1: "html",
-        tech2: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
-        alt2: "tailwind",
-        tech3: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-        alt3: "javascript",
-        link: "https://alqorni30.github.io/landingpage-Tailwindcss/",
-        preview: "Preview"
-    },
-    {
-        id: 4,
-        image: portofolio,
-        alt: "portofolio",
-        title: "Portofolio",
-        desc: " Personal website Portofolio with tailwind css.",
-        tech1: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
-        alt1: "html",
-        tech2: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
-        alt2: "tailwind",
-        tech3: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-        alt3: "javascript",
-        link: "https://portofolio-alqorni.vercel.app/",
-        preview: "Preview"
-    },
-    {
-        id: 5,
-        image: gallery,
-        alt: "gallery",
-        title: "My Gallery Web",
-        desc: "Showing my photography work on the web",
-        tech1: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
-        alt1: "html", 
-        tech2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
-        alt2: "css",
-        tech3: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-        alt3: "javascript",
-        link: "https://gallery-bayo.vercel.app/",
-        preview: "Preview"
-    },
-    {
-        id: 6,
-        image: bimbel,
-        alt: "bimbel",
-        title: "Bimbel Allclass",
-        desc: "Bimbingan Belajar untuk SD,SMP,SMA, dan Gapyear",
-        tech1: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
-        alt1: "html", 
-        tech2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
-        alt2: "css",
-        tech3: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-        alt3: "javascript",
-        tech4: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
-        alt4: "boostrap",
-        link: "https://bimbel-all-class.vercel.app/",
-        preview: "Preview"
-    },
-    {
-        id: 7,
-        image: bayodev,
-        alt: "BayoDev",
-        title: "Personal Website",
-        desc: "Portfolio website to showcase my projects",
-        tech1: react,
-        alt1: "react", 
-        tech2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
-        alt2: "boostrap",
-        tech3: "https://vitejs.dev/logo.svg",
-        alt3: "viteJs",
-        link: "https://web-course-reactvitejs.netlify.app//",
-        preview: "Preview"
-    },
-    {
-        id: 8,
-        image: course,
-        alt: "course",
-        title: "Course online",
-        desc: "Tersedia kelas online untuk upgrade skillmu!!",
-        tech1: react,
-        alt1: "react", 
-        tech2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
-        alt2: "boostrap",
-        tech3: "https://vitejs.dev/logo.svg",
-        alt3: "viteJs",
-        link: "https://web-course-reactvitejs.netlify.app//",
-        preview: "Preview"
-    },
-    {
-        id: 9,
-        image: manga,
-        alt: "manga",
-        title: "Wibu.id",
-        desc: "All Manga with Jikan Api, Search for your favorite manga",
-        tech1: react,
-        alt1: "react", 
-        tech2: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
-        alt2: "tailwind",
-        tech3: "https://vitejs.dev/logo.svg",
-        alt3: "viteJs",
-        tech4: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-        alt4: "javascript",
-        link: "https://manga-api-bayo.vercel.app/",
-        preview: "Preview"
-    },
-    {
-        id: 10,
-        image: notes,
-        alt: "notes",
-        title: "Notes App",
-        desc: "write your notes to remember your daily life here!",
-        tech1: react,
-        alt1: "react", 
-        tech2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
-        alt2: "typescript",
-        tech3: "https://vitejs.dev/logo.svg",
-        alt3: "viteJs",
-        tech4: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
-        alt4: "tailwind",
-        link: "https://notes-app-amber-two.vercel.app/",
-        preview: "Preview"
-    },
-    {
-        id: 11,
-        image: undangan,
-        alt: "UndanganDigital",
-        title: "Undangan Digital",
-        desc: "Make your digital invitation beatiful and interactive",
-        tech1: react,
-        alt1: "react", 
-        tech2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
-        alt2: "typescript",
-        tech3: "https://vitejs.dev/logo.svg",
-        alt3: "viteJs",
-        tech4: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
-        alt4: "tailwind",
-        link: "https://undangan-digital-green.vercel.app/",
-        preview: "Preview"
-    },
+  {
+    id: 2,
+    image: umkm,
+    alt: "umkm",
+    title: "Crispy.Id",
+    desc: "Pesan cemilan favoritmu diwebsite ini",
+    tech1:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    alt1: "html",
+    tech2:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+    alt2: "css",
+    tech3:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    alt3: "javascript",
+    link: "https://bayocrisypy.netlify.app/",
+    preview: "Preview",
+  },
+  {
+    id: 6,
+    image: nft,
+    alt: "nft",
+    title: "Tweak AI",
+    desc: "Company Profile memamerkan Bermacam-macam NFT",
+    tech1:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    alt1: "html",
+    tech2: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    alt2: "tailwind",
+    tech3:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    alt3: "javascript",
+    link: "https://alqorni30.github.io/landingpage-Tailwindcss/",
+    preview: "Preview",
+  },
+  {
+    id: 4,
+    image: portofolio,
+    alt: "portofolio",
+    title: "Portofolio",
+    desc: " Personal website Portofolio with tailwind css.",
+    tech1:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    alt1: "html",
+    tech2: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    alt2: "tailwind",
+    tech3:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    alt3: "javascript",
+    link: "https://portofolio-alqorni.vercel.app/",
+    preview: "Preview",
+  },
+  {
+    id: 5,
+    image: gallery,
+    alt: "gallery",
+    title: "My Gallery Web",
+    desc: "Showing my photography work on the web",
+    tech1:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    alt1: "html",
+    tech2:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+    alt2: "css",
+    tech3:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    alt3: "javascript",
+    link: "https://gallery-bayo.vercel.app/",
+    preview: "Preview",
+  },
+  {
+    id: 6,
+    image: bimbel,
+    alt: "bimbel",
+    title: "Bimbel Allclass",
+    desc: "Bimbingan Belajar untuk SD,SMP,SMA, dan Gapyear",
+    tech1:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    alt1: "html",
+    tech2:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+    alt2: "css",
+    tech3:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    alt3: "javascript",
+    tech4:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+    alt4: "boostrap",
+    link: "https://bimbel-all-class.vercel.app/",
+    preview: "Preview",
+  },
+  {
+    id: 7,
+    image: bayodev,
+    alt: "BayoDev",
+    title: "Personal Website",
+    desc: "Portfolio website to showcase my projects",
+    tech1: react,
+    alt1: "react",
+    tech2:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+    alt2: "boostrap",
+    tech3: "https://vitejs.dev/logo.svg",
+    alt3: "viteJs",
+    link: "https://web-course-reactvitejs.netlify.app//",
+    preview: "Preview",
+  },
+  {
+    id: 8,
+    image: course,
+    alt: "course",
+    title: "Course online",
+    desc: "Tersedia kelas online untuk upgrade skillmu!!",
+    tech1: react,
+    alt1: "react",
+    tech2:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+    alt2: "boostrap",
+    tech3: "https://vitejs.dev/logo.svg",
+    alt3: "viteJs",
+    link: "https://web-course-reactvitejs.netlify.app//",
+    preview: "Preview",
+  },
+  {
+    id: 9,
+    image: manga,
+    alt: "manga",
+    title: "Wibu.id",
+    desc: "All Manga with Jikan Api, Search for your favorite manga",
+    tech1: react,
+    alt1: "react",
+    tech2: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    alt2: "tailwind",
+    tech3: "https://vitejs.dev/logo.svg",
+    alt3: "viteJs",
+    tech4:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    alt4: "javascript",
+    link: "https://manga-api-bayo.vercel.app/",
+    preview: "Preview",
+  },
+  {
+    id: 10,
+    image: notes,
+    alt: "notes",
+    title: "Notes App",
+    desc: "write your notes to remember your daily life here!",
+    tech1: react,
+    alt1: "react",
+    tech2:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    alt2: "typescript",
+    tech3: "https://vitejs.dev/logo.svg",
+    alt3: "viteJs",
+    tech4: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    alt4: "tailwind",
+    link: "https://notes-app-amber-two.vercel.app/",
+    preview: "Preview",
+  },
+  {
+    id: 11,
+    image: undangan,
+    alt: "UndanganDigital",
+    title: "Undangan Digital",
+    desc: "Make your digital invitation beatiful and interactive",
+    tech1: react,
+    alt1: "react",
+    tech2:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    alt2: "typescript",
+    tech3: "https://vitejs.dev/logo.svg",
+    alt3: "viteJs",
+    tech4: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    alt4: "tailwind",
+    link: "https://undangan-digital-green.vercel.app/",
+    preview: "Preview",
+  },
+  {
+    id: 12,
+    image: weblkg,
+    alt: "web-lkg",
+    title: "Layanan kolaborasi global",
+    desc: "PT. Layanan Kolaborasi Global company profile",
+    tech1: "https://cdn.worldvectorlogo.com/logos/next-js.svg",
+    alt1: "nextJs",
+    tech2:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    alt2: "typescript",
+    tech3: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    alt3: "tailwind",
+    link: "https://web-lkg.netlify.app/",
+    preview: "Preview",
+  },
 ];
 
 export const iconSkills = [
-    {
-        id: 1,
-        techImage: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
-        tectAlt: "typescript",
-    },
-    {
-        id: 2,
-        techImage: react,
-        tectAlt: "react",
-    },
-    {
-        id: 3,
-        techImage: "https://vitejs.dev/logo.svg",
-        tectAlt: "viteJs",
-    },
-    {
-        id: 4,
-        techImage: "https://cdn.worldvectorlogo.com/logos/nextjs-2.svg",
-        tectAlt: "nextJs",
-    },
-    {
-        id: 5,
-        techImage: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
-        tectAlt: "tailwind",
-    },
-    {
-        id: 6,
-        techImage: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
-        tectAlt: "nodeJs",
-    },
-    {
-        id: 8,
-        techImage: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-        tectAlt: "javascript",
-    },
-    {
-        id: 9,
-        techImage: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
-        tectAlt: "boostrap",
-    },
-    {
-        id: 10,
-        techImage: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
-        tectAlt: "html",
-    },
-    {
-        id: 11,
-        techImage: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
-        tectAlt: "css",
-    },
-    {
-        id: 12,
-        techImage: "https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg",
-        tectAlt: "php",
-    },
-    {
-        id: 12,
-        techImage: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
-        tectAlt: "mysql",
-    },
+  {
+    id: 1,
+    techImage:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    tectAlt: "typescript",
+  },
+  {
+    id: 2,
+    techImage: react,
+    tectAlt: "react",
+  },
+  {
+    id: 3,
+    techImage: "https://vitejs.dev/logo.svg",
+    tectAlt: "viteJs",
+  },
+  {
+    id: 4,
+    techImage: "https://cdn.worldvectorlogo.com/logos/nextjs-2.svg",
+    tectAlt: "nextJs",
+  },
+  {
+    id: 5,
+    techImage:
+      "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    tectAlt: "tailwind",
+  },
+  {
+    id: 6,
+    techImage:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+    tectAlt: "nodeJs",
+  },
+  {
+    id: 8,
+    techImage:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    tectAlt: "javascript",
+  },
+  {
+    id: 9,
+    techImage:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+    tectAlt: "boostrap",
+  },
+  {
+    id: 10,
+    techImage:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    tectAlt: "html",
+  },
+  {
+    id: 11,
+    techImage:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+    tectAlt: "css",
+  },
+  {
+    id: 12,
+    techImage:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg",
+    tectAlt: "php",
+  },
+  {
+    id: 12,
+    techImage:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+    tectAlt: "mysql",
+  },
 ];
